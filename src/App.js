d45bf90700526d34cbe0c3e915793a34d4179949
Bishes Adhikari <specialbishes9@gmail.com>
2024-12-26 14:17:20 +1100

    initial push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import "./App.css";
+// 1abe6a66
+
+const apiUrl = "http://www.omdbapi.com?apikey=1abe6a66";
+
+const App = () => {
+  useEffect(() => {}, []);
+  return <div className="App"></div>;
+};
+
+export default App;
